dcc9a776f3c521157d4f9b989153c25512358f90
Vlada <lartwoods@gmail.com>
2022-12-28 23:43:35 +0300

    added task1

--- task1/function_input_script.js
+++ task1/function_input_script.js
@@ -1,41 +1,30 @@
-let myNumbers = [];
-let enter = document.getElementById("inputNumber");
-let nexT = document.getElementById("button_next");
+let x = 0;
+let Mass = [];
 
-nexT.addEventListener("click", function () {
-  let a = enter.value;
-  myNumbers.push(a);
-  enter.value = "";
-});
+function add_element_to_array() {
+  Mass[x] = document.getElementById("text1").value;
 
-let ready = document.getElementById("input_ready");
-let myMass = document.getElementsByClassName("array");
-let sumRes = document.getElementsByClassName("sum");
-
-ready.addEventListener("click", function () {
-  myMass.innerHTML = myNumbers.join("");
-});
-ready.addEventListener("click", function () {
-  let counter = 0;
-  for (let number of myNumbers) {
-    counter = counter + +number;
-    sumRes.innerHTML = counter;
-  }
-});
-
-document.getElementById("inputReady").onclick = myClick;
-function myClick() {
-  for (j = 0; i < myNumbers.lenght; j++) {
-    for (let i = 0; i < myNumbers.lenght - 1; i++) {
-      let a = myNumbers[i],
-        b = myNumbers[i + 1];
-      if (a > b) {
-        let temp = a;
-        a = b;
-        b = temp;
-      }
-    }
-  }
-  myNumbers[i] = a;
-  myNumbers[i + 1] = b;
+  x++;
+  document.getElementById("text1").value = "";
 }
+
+function display_array() {
+  let e = "<hr/>";
+
+  for (let y = 0; y < Mass.length; y++) {
+    e += Mass[y] + "<br/>";
+  }
+  document.getElementById("Result").innerHTML = e;
+}
+button2.addEventListener("click", function () {
+  let counter = 0;
+  for (let number of Mass) {
+    counter = counter + +number;
+    sum.innerHTML = counter;
+  }
+});
+let sortMass = Mass.sort(function (a, b) {
+  if (a < b) return -1;
+  console.log(a, b);
+});
+console.log(sortMass);
